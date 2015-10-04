eccbd4199746e361e72a5d11f72d1585da31d599
oldlaptop <oldlaptop654@aol.com>
2015-10-04 16:11:41 -0400

    botulism.sh actually behaves
     wait for all evloops to die, and kill them on recieving SIGTERM

--- botulism.sh
+++ botulism.sh
@@ -15,6 +15,17 @@ export PATH="${PATH}:/usr/games"
 # botulism installation directory, cwd by default
 export PREFIX="$(pwd)"
 
+EVLOOP_PIDS=
+
+# Kill all event loops
+die()
+{
+	for process in ${EVLOOP_PIDS}
+	do
+		kill ${process}
+	done
+}
+
 # Spawn event loops on all channels
 for channel in ${CHANNELS}
 do
@@ -23,5 +34,14 @@ do
 	# and dumping output to each channel, perhaps using tee? As it stands
 	# now, multiple channels haven't really been tested in the first place.
 	tail -fn1 "${IRCDIR}/${SERVER}/${channel}/out" | sh "${PREFIX}/evloop.sh" > "${IRCDIR}/${SERVER}/${channel}/in" &
+	EVLOOP_PIDS="${EVLOOP_PIDS} $!"
 	tail -fn1 "${IRCDIR}/${SERVER}/out" | sh "${PREFIX}/evloop.sh" > "${IRCDIR}/${SERVER}/${channel}/in" &
+	EVLOOP_PIDS="${EVLOOP_PIDS} $!"
 done
+
+# Idle, waiting to be SIGTERMed
+trap die TERM
+wait
+
+# All event loops are dead
+printf "%s" "$0: all event loops died, terminating"
